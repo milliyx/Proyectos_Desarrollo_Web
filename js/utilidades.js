6e1dc29c749d71b3b6d2b61fd9560fb823ef1a7c
milliyx <143279616+milliyx@users.noreply.github.com>
2023-10-16 22:54:41 -0600

    [MOD]16092023
    ultimas cositas que pude agregar ):

--- js/utilidades.js
+++ js/utilidades.js
@@ -8,3 +8,13 @@ window.addEventListener('scroll', function() {
     document.querySelector('.black-overlay').style.display = 'none';
   }
 });
+// Obtén el enlace original y el nuevo enlace por su ID
+const reservacionLink = document.getElementById('reservacion-link');
+const nuevoEnlace = document.getElementById('nuevo-enlace');
+
+// Agrega un controlador de eventos al enlace original
+reservacionLink.addEventListener('click', function (event) {
+    event.preventDefault(); // Previene el comportamiento predeterminado del enlace
+    reservacionLink.style.display = 'none'; // Oculta el enlace original
+    nuevoEnlace.style.display = 'block'; // Muestra el nuevo enlace
+});
